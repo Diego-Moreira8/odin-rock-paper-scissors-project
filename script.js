59445e854c0acb3eeb6f5262d53d5074a85f68fe
Diego Moreira <diego.moreira529@gmail.com>
2022-06-14 15:07:03 -0300

    Created round()

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function computerSelection() {
   let play = "";
   switch (Math.floor(Math.random() * 3 + 1)) {
     case 1:
-      play = "Rock";
+      play = "rock";
       break;
     case 2:
-      play = "Paper";
+      play = "paper";
       break;
     case 3:
-      play = "Scissors";
+      play = "scissors";
       break;
   }
   return play;
@@ -21,3 +21,23 @@ function playerSelection() {
   } while (play !== "rock" && play !== "paper" && play !== "scissors");
   return play;
 }
+
+function round(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "Computer wins: paper eats rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "Player wins: rock breaks scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "Player wins: paper eats rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "Computer wins: scissors cuts paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "Computer wins: rock breaks scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "Player wins: scissors cuts paper";
+  }
+}
+
+console.log(round(computerSelection(), computerSelection()));
