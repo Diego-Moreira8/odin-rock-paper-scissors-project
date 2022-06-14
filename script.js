46f6a9f7cc612f37be06603beb4c77543bd78cb4
Diego Moreira <diego.moreira529@gmail.com>
2022-06-14 14:35:16 -0300

    Created playerSelection()

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function computerPlay() {
+function computerSelection() {
   let play = "";
   switch (Math.floor(Math.random() * 3 + 1)) {
     case 1:
@@ -14,7 +14,10 @@ function computerPlay() {
   return play;
 }
 
-// test function
-// for (let i = 0; i < 1000; i++) {
-//   console.log(computerPlay());
-// }
+function playerSelection() {
+  let play = "";
+  do {
+    play = prompt("Enter your play:").toLowerCase();
+  } while (play !== "rock" && play !== "paper" && play !== "scissors");
+  return play;
+}
