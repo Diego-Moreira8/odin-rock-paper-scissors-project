819e18668ffbc25944d9a9bb04905198023a14bf
Diego Moreira <diego.moreira529@gmail.com>
2022-06-14 13:47:01 -0300

    Created and tested computerPlay()

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello ?World!");
+function computerPlay() {
+  let play = "";
+  switch (Math.floor(Math.random() * 3 + 1)) {
+    case 1:
+      play = "Rock";
+      break;
+    case 2:
+      play = "Paper";
+      break;
+    case 3:
+      play = "Scissors";
+      break;
+  }
+  return play;
+}
+
+// test function
+// for (let i = 0; i < 1000; i++) {
+//   console.log(computerPlay());
+// }
